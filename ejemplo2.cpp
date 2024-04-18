b0742664003442835dbfeee94baa0785adec5f88
Djave17 <dsancheza@uamv.edu.ni>
2024-04-18 12:20:37 -0600

    prueba

---
+++ ejemplo2.cpp
@@ -0,0 +1,18 @@
+#include <iostream> 
+#include <math.h>
+using namespace std; 
+
+int main (){
+    int num, ant, cua; 
+    cout << "Dime un numero" << endl; 
+    cin >> num; 
+    ant  = num -1; 
+    cua = pow(ant, 2); 
+    cout << "El numero menos 1 elevado al cuadrado es: " << num << " es: " << cua << endl; 
+    return 0; 
+
+
+
+
+
+}

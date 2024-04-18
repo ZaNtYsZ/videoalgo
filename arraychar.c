a0a53bb50f3be8d5c16adf31d403c07baea9c108
Djave17 <dsancheza@uamv.edu.ni>
2024-04-18 12:05:50 -0600

    Anadir

---
+++ arraychar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    char vocales [5];
+
+    vocales[0] = 'a'; 
+    vocales[1] = 'e'; 
+    vocales[2] = 'i'; 
+    vocales[3]= 'o'; 
+    vocales[4] = 'u'; 
+
+    printf("Vocal # 1: %c \n", vocales[0]); 
+    printf("Vocal # 2: %c \n", vocales[1]); 
+    printf("Vocal # 3: %c \n", vocales[2]); 
+    printf("Vocal # 4: %c \n", vocales[3]); 
+    printf("Vocal # 5: %c \n", vocales[4]); 
+
+
+    return 0;
+}
